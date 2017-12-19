0960b2c72a4ff487ee8bb2565f64eaddc50e6f9d
lhj <plissmile@gmail.com>
2017-12-19 23:15:47 +0800

    update

--- ensemble.py
+++ ensemble.py
@@ -1,4 +1,5 @@
 import pickle
+import numpy as np
 
 class AdaBoostClassifier:
     '''A simple AdaBoost Classifier.'''
@@ -10,6 +11,10 @@ class AdaBoostClassifier:
             weak_classifier: The class of weak classifier, which is recommend to be sklearn.tree.DecisionTreeClassifier.
             n_weakers_limit: The maximum number of weak classifier the model can use.
         '''
+        self.weak_classifier = weak_classifier
+        self.n_weakers_limit = n_weakers_limit
+        self.classifiers_ = []
+        self.alpha_ = []
         pass
 
     def is_good_enough(self):
@@ -23,8 +28,34 @@ class AdaBoostClassifier:
             X: An ndarray indicating the samples to be trained, which shape should be (n_samples,n_features).
             y: An ndarray indicating the ground-truth labels correspond to X, which shape should be (n_samples,1).
         '''
-        pass
 
+        num_samples, num_features = X.shape
+        # Initial weights the sample weights are initialized to 1 / num_samples
+        sample_weight = np.ones(num_samples) * (1 / num_samples)
+
+        for iboost in range(self.n_weakers_limit):
+            estimator = self.weak_classifier
+            estimator.fit(X, y, sample_weight=sample_weight)
+            print("Finish No." + str(iboost + 1) + " weak classifier.")
+            self.classifiers_.append(estimator)
+            y_predict = estimator.predict(X)
+            y_predict = y_predict.reshape((len(y_predict), 1))
+
+            incorrect = y_predict != y
+            # Error fraction
+            estimator_error = np.sum(sample_weight.dot(incorrect))
+
+            # Stop if classification is perfect
+            if estimator_error <= 0:
+                return self
+
+            alpha = np.log(1. / (estimator_error) - 1) / 2.
+            self.alpha_.append(alpha)
+
+            for j in range(len(sample_weight)):
+                sample_weight[j] = sample_weight[j] * np.exp(-y[j]*alpha*y_predict[j])
+            sample_weight /= np.sum(sample_weight)
+        return self
 
     def predict_scores(self, X):
         '''Calculate the weighted sum score of the whole base classifiers for given samples.
@@ -47,7 +78,11 @@ class AdaBoostClassifier:
         Returns:
             An ndarray consists of predicted labels, which shape should be (n_samples,1).
         '''
-        pass
+        pred = sum(classifier.predict(X).T * w
+                   for classifier, w in zip(self.classifiers_,
+                                           self.alpha_))
+        pred = np.sign(pred)
+        return pred
 
     @staticmethod
     def save(model, filename):
--- train.py
+++ train.py
@@ -1,6 +1,39 @@
-
+import numpy as np
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+from ensemble import *
+from sklearn import tree
 
 if __name__ == "__main__":
-    # write your code here
-    pass
+    face_features = np.load("./datasets/face_features.npy")
+    nonface_features = np.load("./datasets/nonface_features.npy")
+
+    num_face_sample, num_face_feature = face_features.shape
+    num_nonface_sample, num_nonface_feature = nonface_features.shape
+
+    positive_label = [np.ones(1) for i in range(num_face_sample)]
+    negative_label = [-np.ones(1) for i in range(num_nonface_sample)]
+
+    positive_samples = np.concatenate((face_features, positive_label), axis=1)
+    negative_samples = np.concatenate((nonface_features, negative_label), axis=1)
+
+    training_size = 800
+    rate = 0.5
+    data = np.concatenate((positive_samples[:int(training_size*rate), :],
+                                 negative_samples[:int(training_size*(1-rate)), :]),
+                                axis=0)
+    X = data[:training_size, :num_face_feature]
+    y = data[:training_size, -1]
+    X_train, X_validation, y_train, y_validation = train_test_split(X, y, test_size=0.33, random_state=42)
+    y_train = y_train.reshape((len(y_train), 1))
+    y_validation = y_validation.reshape((len(y_validation), 1))
+    print(X_train.shape, y_train.shape, X_validation.shape, y_validation.shape)
+
+    weak_classifier = tree.DecisionTreeClassifier(criterion='entropy', max_depth=4)
+    classifier = AdaBoostClassifier(weak_classifier, 2)
+    classifier.fit(X_train, y_train)
+
+    Y_pred = classifier.predict(X_validation)
+    target_names = ['face', 'non face']
+    print(classification_report(y_validation, Y_pred, target_names=target_names, digits=4))
 
